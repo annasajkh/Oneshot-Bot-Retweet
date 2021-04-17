8a49f72e6b2266d5d576ac462b52186b5ed3ed26
Annas <61279556+annasajkh@users.noreply.github.com>
2021-04-18 06:13:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
 import tweepy
-import os
+import time
 
-banned = ["fortnite","call of duty","callofduty","dungeonsanddragons","sorare","#dnd","oneshotleague","callofcthulhu"]
+banned_words = ["fortnite","call of duty","callofduty","dungeonsanddragons",
+"sorare","dnd","oneshotleague","callofcthulhu"]
+
+oneshot_words = ["niko","alula","calamus","prototype","silver","george","lamplighter","ling","kip",
+                "kelvin","magpie","maize","prophet","rue","world machine"]
 
 class Listener(tweepy.StreamListener):
-
     def on_status(self, status):
         try:
             if hasattr(status, "retweeted_status"):
-                return
-            if status.retweeted:
-                return
-            if status.is_quote_status:
-                return
-            if status.in_reply_to_status_id is not None:
-                return
+                raise Exception("already retweeted")
 
-            for word in banned:
-                if word in status.text.lower():
-                    return
+            if hasattr(status, "quoted_status"):
+                raise Exception("is quoted tweet")
+
+            if status.retweeted:
+                raise Exception("is retweeted status")
+
+            if status.in_reply_to_status_id is not None:
+                raise Exception("is a comment")
+                
+            for banned_word in banned_words:
+                if banned_word in status.text.lower():
+                    contain_oneshot_word = False
+                    for oneshot_word in oneshot_words:
+                        if oneshot_word in status.text.lower():
+                            contain_oneshot_word = True
+                            break
+                    if not contain_oneshot_word:
+                        raise Exception("banned word!")
 
             blocks = twitter.blocks()
             mutes = twitter.mutes()
 
             for blocked in blocks:
                 if blocked.screen_name == status.user.screen_name:
-                    return
+                    raise Exception("blocked!")
+
             for mute in mutes:
                 if mute.screen_name == status.user.screen_name:
-                    return
+                    raise Exception("muted!")
+            
             twitter.retweet(status.id)
             twitter.create_favorite(status.id)
-        except:
-            print("can't retweet")
+        except Exception as e:
+            print(e)
 
-auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"])
-auth.set_access_token(os.environ["ACCESS_TOKEN"],os.environ["ACCESS_TOKEN_SECRET"])
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("","")
 
 twitter = tweepy.API(auth)
 
 listener = Listener()
-stream = tweepy.Stream(auth, listener)
-stream.filter(track=["#oneshotgame"])
+stream = tweepy.Stream(auth, listener,tweet_mode="extended")
+
+while True:
+    try:
+        stream.filter(track=["#oneshotgame"])
+    except Exception as e:
+        print(e)
