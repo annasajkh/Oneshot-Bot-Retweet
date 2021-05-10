905c881becdbf8b4c420b9520e71155a7d1536fc
Annas <61279556+annasajkh@users.noreply.github.com>
2021-05-10 20:53:33 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,23 +10,36 @@ oneshot_words = ["niko","alula","calamus","prototype","silver","george","lamplig
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
         try:
+
+            hashtags = status.entities["hashtags"]
+            hashtag_texts = [x["text"].lower() for x in hashtags]
+
             if hasattr(status, "retweeted_status"):
                 raise Exception("already retweeted")
 
             if hasattr(status, "quoted_status"):
+                if "oneshotgame" in hashtag_texts:
+                    twitter.create_favorite(status.id)
+                    twitter.retweet(status.id)
                 raise Exception("is quoted tweet")
 
             if status.retweeted:
                 raise Exception("is retweeted status")
 
             if status.in_reply_to_status_id is not None:
+                twitter.create_favorite(status.in_reply_to_status_id)
+                twitter.retweet(status.in_reply_to_status_id)
                 raise Exception("is a comment")
+
                 
             for banned_word in banned_words:
-                if banned_word in status.text.lower():
+
+                if banned_word in hashtag_texts or banned_word in status.text:
+
                     contain_oneshot_word = False
+
                     for oneshot_word in oneshot_words:
-                        if oneshot_word in status.text.lower():
+                        if oneshot_word in status.text:
                             contain_oneshot_word = True
                             break
                     if not contain_oneshot_word:
@@ -48,13 +61,13 @@ class Listener(tweepy.StreamListener):
         except Exception as e:
             print(e)
 
-auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"],os.environ["CONSUMER_SECRET"])
+auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"], os.environ["CONSUMER_SECRET"])
 auth.set_access_token(os.environ["ACCESS_TOKEN"],os.environ["ACCESS_TOKEN_SECRET"])
 
 twitter = tweepy.API(auth)
 
 listener = Listener()
-stream = tweepy.Stream(auth, listener,tweet_mode="extended")
+stream = tweepy.Stream(auth, listener)
 
 while True:
     try:
